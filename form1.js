3145f94363a0a328e2483675f2c344cdd9d6474c
asad <shaikhasad183@gmail.com>
2024-05-19 20:06:18 +0500

    final12

--- form1.js
+++ form1.js
@@ -149,8 +149,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
       loader.style.display = 'block';
-      // const paymentSuccessful = await makePayment(0.01);
-      const paymentSuccessful = true;
+      const paymentSuccessful = await makePayment(0.01);
+      // const paymentSuccessful = true;
 
 if(paymentSuccessful) { 
       const response = await fetch("https://api.quinielacrypto.com/saveData", {
